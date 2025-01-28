82015a5a3e14f6a76e82166eab69abc0cf76bd1b
Davide Mauri <damauri@microsoft.com>
2025-01-28 14:12:06 -0800

    added note about using MSI

--- 02-load-table.sql
+++ 02-load-table.sql
@@ -21,6 +21,9 @@ go
 	Create database scoped credential and external data source.
 	File is assumed to be in a path like: 
 	https://<myaccount>.blob.core.windows.net/playground/wikipedia/vector_database_wikipedia_articles_embedded.csv
+
+	Please note that it is recommened to avoid using SAS tokens: the best practice is to use Managed Identity as described here:
+	https://learn.microsoft.com/en-us/sql/relational-databases/import-export/import-bulk-data-by-using-bulk-insert-or-openrowset-bulk-sql-server?view=sql-server-ver16#bulk-importing-from-azure-blob-storage
 */
 create database scoped credential [openai_playground]
 with identity = 'SHARED ACCESS SIGNATURE',
